7bd538e9e558e967adc94fcf8e080b3a92efb736
Tushar Kashyap <tusharkashyap888@gamil.com>
2024-08-17 01:39:55 +0530

    rest api project 98% done

--- 05-REST-COUNTRIES-API/app.js
+++ 05-REST-COUNTRIES-API/app.js
@@ -1,25 +1,56 @@
 const countriesContainer = document.querySelector(".countries-container");
+const filterByRegion = document.querySelector(".filter-by-region");
+const searchInput = document.querySelector(".search-container input");
+const themeChanger = document.querySelector(".theme-changer");
+
+let allCountriesData 
+
 
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
-  .then((data) => {
-    data.forEach((country) => {
-      // console.log(country);
-      const countryCard = document.createElement("a");
-      countryCard.classList.add("country-card");
-      countryCard.href = `/country.html?name=${country.name.common}`;
-      countryCard.innerHTML = `
-   <img src="${country.flags.svg}" alt="flag">
-   <div class="card-text">
-     <h3 class="card-title">${country.name.common}</h3>
-     <p><b>Population: </b>${country.population.toLocaleString("en-IN")}</p>
-     <p><b>Region: </b>${country.region}</p>
-     <p><b>Capital: </b>${country.capital}</p>
+  .then((data)=>{
+    randerCountries(data)
+    allCountriesData = data
+  });  
+
+
+
+filterByRegion.addEventListener("change",(e)=>{
+ fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+  .then((res) => res.json())
+  .then(randerCountries);
+})
+  
+
+function randerCountries(data){
+  countriesContainer.innerHTML = ``
+  data.forEach((country) => {
+   const countryCard = document.createElement("a");
+   countryCard.classList.add("country-card");
+   countryCard.href = `/country.html?name=${country.name.common}`;
+   countryCard.innerHTML = `
+  <img src="${country.flags.svg}" alt="flag">
+  <div class="card-text">
+   <h3 class="card-title">${country.name.common}</h3>
+   <p><b>Population: </b>${country.population.toLocaleString("en-IN")}</p>
+   <p><b>Region: </b>${country.region}</p>
+   <p><b>Capital: </b>${country.capital}</p>
   </div>
 `;
-      countriesContainer.appendChild(countryCard);
-    });
-  });
+   countriesContainer.appendChild(countryCard);
+ });
+}
 
 
-  
+searchInput.addEventListener("input",(e)=>{
+  // console.log(e.target.value);
+  // console.log(allCountriesData);
+ const filteredCountries =  allCountriesData.filter((country)=> country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+ console.log(filteredCountries);
+ randerCountries(filteredCountries)
+})
+
+
+themeChanger.addEventListener("click",()=>{
+  document.body.classList.toggle("dark")
+})
